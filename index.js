afb1e5b2dcff12f5e3aa5a6a91ee1ea229f42d41
Diana Marcelin <marcelindiana@yahoo.com>
2022-09-19 11:39:51 -0400

    created first route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to API!" });
+});
+
 app.listen(3000, () => {
   console.log(`Listening!`);
 });
